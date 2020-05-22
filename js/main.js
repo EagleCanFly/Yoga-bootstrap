7e47f1f346079faf324b0262244b522c7b8dfbdd
Orlov Ilya <EagleCanFly>
2020-05-22 16:31:36 +0300

    22/05

--- js/main.js
+++ js/main.js
@@ -1,13 +1,55 @@
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        center: true,
-        items:2,
-        loop:true,
-        margin:200,
-        responsive:{
-            600:{
-                items:2
-            }
-        }
-    });
+	$("#slider-1").owlCarousel({
+		center: true,
+		items: 2,
+		loop: true,
+		margin: 200,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+				margin: 50,
+			},
+			900: {
+				items: 2,
+				margin: 200,
+			},
+		},
+	}),
+		$("#slider-2").owlCarousel({
+			center: true,
+			items: 2,
+			loop: true,
+			margin: 200,
+			responsive: {
+				0: {
+					items: 1,
+				},
+				600: {
+					items: 1,
+					margin: 50,
+				},
+				900: {
+					items: 2,
+					margin: 200,
+				},
+			},
+		}),
+		$("#slider-3").owlCarousel({
+			items: 1,
+			loop: true,
+			margin: 50,
+        }),
+        $("#slider-4").owlCarousel({
+			items: 1,
+			loop: true,
+			margin: 50,
+        }),
+        $("#slider-5").owlCarousel({
+			items: 1,
+			loop: true,
+			margin: 50,
+		});
 });
